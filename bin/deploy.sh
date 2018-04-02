81d45c49a70fc1e43161ef763ec024f103258d1f
hhw2018 <2633050085@qq.com>
2018-04-02 23:38:16 +0800

    deploy work done.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,57 +1,110 @@
-#!/usr/bin/bash
-
+#!/usr/bin/bash 
+NAME=${0##*/}
+    
 function usage {
-    local name=${0##*\/}
-    echo "Usage: $name -f host_list_file"
+cat <<EOF
+Usage: $NAME -f host_file|-h host_list [-d path]
+  host_file: file with host ips separated by new-line char.
+  host_list: list of host ips separated by space.
+  path: work home where the framework will be installed(/var/log by default).
+EOF
     exit 1
 }
 
 # Register the framework env vars and lib functions
-function config_framework {
+function config {
     local path=${0%\/*}
 
-    # Get the framework dir list and register them
     export MOON_BSHAUTO_HOME=$(cd $path/../; pwd)
-    export MOON_BSHAUTO_BIN=$MOON_BSHAUTO_HOME/bin
     export MOON_BSHAUTO_FW_LIB=$MOON_BSHAUTO_HOME/lib/framework
     export MOON_BSHAUTO_FW_CONF=$MOON_BSHAUTO_HOME/conf/framework
      
-    # Register the framework env vars
-    local file=""
-    for file in $MOON_BSHAUTO_FW_CONF/*; do
-        . $file
-    done
-
-    # Load the framework libs
-    for file in $MOON_BSHAUTO_FW_LIB/*; do
-        . $file
-    done
+    # Register the framework env vars as well as libs 
+    . $MOON_BSHAUTO_FW_LIB/remote.bshlib
+    . $MOON_BSHAUTO_FW_CONF/expect.cfg
 }
 
 function parse_arguments {
+    local f=0
+    local h=0
+
+    # The 1st option must be -f or -h
     case $1 in
+        -f)
+            shift
+            f=1
+            [[ ! -f "$1" ]] \
+                && echo "$NAME: Cannot access $1." \
+                && usage
+            host_list=$(cat $1)
+            [[ -z "$host_list" ]] \
+                && echo "$NAME: Empty file." \
+                && usage
+            shift
+            ;;
+
         -h)
             shift
-            gfile="$@"
+            h=1
+            local cnt=$(egrep -bow '\-d' <<< "$*" | cut -d':' -f1 | head -1)
+            local args="$*"
+            ((cnt == 0)) && host_list="$*" || host_list="${args:0:$cnt}"
+            [[ -z "$host_list" ]] \
+                && echo "$NAME: No host is specified." \
+                && usage
+            cnt=$(echo "$host_list" | wc -w)
+            shift $cnt
             ;;
+
          *)
             usage
             ;;
     esac
 
-    [[ -z "$gfile" ]]  && usage
+    # -d must be the 1st if there are more arguments left.
+    if (($# > 0)); then
+        case $1 in
+            -d)
+                shift
+                work_home=$1
+                [[ ! -d "$work_home" ]] \
+                    && echo "$NAME: A work home path needs to be specified." \
+                    && usage
+                ;;
+    
+            *)
+                echo "error: $1"
+                usage
+                ;;
+        esac
+    fi
+
+    (( f + h != 1)) && usage
 }
 
-gfile=""
+host_list=""
+work_home="/var/log"
 
 function main {
     parse_arguments $@
-    config_framework
+    config
+
+    cd $MOON_BSHAUTO_HOME
 
     local host=""
-    while read line; do
-        echo $host
-    done < $gfile
+    for host in $host_list; do
+        echo "Host: $host, deploying..."
+        (
+        cmd="mkdir -p $work_home/moon_bshauto"
+        rcml_host $host $cmd
+        assert $? -eq 0
+
+        cd $MOON_BSHAUTO_HOME
+        cmd="scp -rp bin lib tests tools conf $MOON_BSHAUTO_EXP_USER@$host:$work_home/moon_bshauto/"
+        rcml "$cmd"
+        )
+        assert $? -eq 0
+    done 
 }
 
 main $@
